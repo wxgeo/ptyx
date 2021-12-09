6059b5b1dec4a495ce8069314a75622c489a5bd5
Nicolas Pourcelot <nicolas.pourcelot@gmail.com>
2021-12-09 23:07:39 +0100

    [autoqcm] First draft of `autoqcm make` command.

--- ptyx/extensions/autoqcm/compile/make.py
+++ ptyx/extensions/autoqcm/compile/make.py
@@ -13,13 +13,36 @@ def make(pth: str = '.', num: int = 1) -> None:
     """
     pth = Path(pth).resolve()
     if pth.suffix != ".ptyx":
-        ptyx_files = list(pth.glob("*.ptyx"))
+        all_ptyx_filenames = list(pth.glob("*.ptyx"))
     if len(ptyx_file) == 0:
         raise FileNotFoundError(f"No .ptyx file found in '{pth}'.")
     elif len(ptyx_file) > 1:
         raise FileNotFoundError(f"Several .ptyx file found in '{pth}', I don't know which to chose.")
-    ptyx_file = ptyx_files[0]
-    compiler.read_file(ptyx_file)
+    ptyx_filename = all_ptyx_filenames[0]
+    # Read pTyX file.
+    print(f"Reading {ptyx_filename}...")
+    compiler.read_file(ptyx_filename)
+    # Parse #INCLUDE tags, load extensions if needed, read seed.
     compiler.preparse()
-    #
-    raise NotImplementedError
+
+    # Generate syntax tree.
+    # The syntax tree is generated only once, and will then be used
+    # for all the following compilations.
+    compiler.generate_syntax_tree()
+
+    # Compile and generate output files (tex or pdf)
+    filenames, output_name, nums = make_files(ptyx_filename, **vars(options))
+
+    # Keep track of the seed used.
+    seed_value = compiler.seed
+    seed_file_name = os.path.join(os.path.dirname(output_name), ".seed")
+    with open(seed_file_name, "w") as seed_file:
+        seed_file.write(str(seed_value))
+
+    filenames, output_name, nums2 = make_files(
+        ptyx_filename, correction=True, _nums=nums
+    )
+    assert nums2 == nums, repr((nums, nums2))
+
+    compiler.close()
+
--- ptyx/extensions/autoqcm/tests/test_autoqcm.py
+++ ptyx/extensions/autoqcm/tests/test_autoqcm.py
@@ -10,7 +10,7 @@ from pathlib import Path
 import atexit
 
 from ptyx.latexgenerator import Compiler, Node
-from ptyx.extensions.autoqcm.compile.ptyx2latex import  SameAnswerError
+from ptyx.extensions.autoqcm.compile.ptyx2latex import SameAnswerError
 
 
 TEST_DIR = Path(__file__).parent.resolve()
@@ -30,11 +30,11 @@ def load_ptyx_file(filename):
 
 
 def test_MCQ_basics():
-    c = load_ptyx_file('partial-test.ptyx')
-    assert 'VERSION' in c.syntax_tree_generator.tags
-    assert 'VERSION' in c.latex_generator.parser.tags
-    assert 'END_QCM' in c.syntax_tree_generator.tags
-    assert 'END_QCM' in c.latex_generator.parser.tags
+    c = load_ptyx_file("partial-test.ptyx")
+    assert "VERSION" in c.syntax_tree_generator.tags
+    assert "VERSION" in c.latex_generator.parser.tags
+    assert "END_QCM" in c.syntax_tree_generator.tags
+    assert "END_QCM" in c.latex_generator.parser.tags
     c.generate_syntax_tree()
     header_found = False
     mcq_found = False
@@ -42,108 +42,110 @@ def test_MCQ_basics():
     for child in c.syntax_tree.children:
         if isinstance(child, Node):
             name = child.name
-            if name == 'QCM_HEADER':
+            if name == "QCM_HEADER":
                 header_found = True
-            elif child.name == 'QCM':
+            elif child.name == "QCM":
                 mcq_found = True
-            elif name == 'SEED':
+            elif name == "SEED":
                 seed_found = True
-    assert not seed_found # `#SEED` is parsed and removed before generating the syntax tree.
+    assert not seed_found  # `#SEED` is parsed and removed before generating the syntax tree.
     assert header_found
     assert mcq_found
-    #TODO: add tests
+    # TODO: add tests
     latex = c.get_latex()
-    #TODO: add tests
+    # TODO: add tests
     assert "Jean de la Fontaine" in latex
 
 
 def test_MCQ_shuffling():
-    c = load_ptyx_file('test_shuffle.ptyx')
+    c = load_ptyx_file("test_shuffle.ptyx")
     c.generate_syntax_tree()
     root = c.syntax_tree
     assert isinstance(root, Node)
-    assert root.name == 'ROOT'
-    assert repr(root) == f'<Node ROOT at {hex(id(root))}>'
+    assert root.name == "ROOT"
+    assert repr(root) == f"<Node ROOT at {hex(id(root))}>"
     header = root.children[0]
     assert isinstance(header, Node)
-    assert header.name == 'QCM_HEADER'
+    assert header.name == "QCM_HEADER"
     mcq = root.children[-2]
     assert isinstance(mcq, Node)
-    assert mcq.name == 'QCM'
+    assert mcq.name == "QCM"
     section = mcq.children[-1]
     assert isinstance(section, Node)
-    assert section.name == 'SECTION'
+    assert section.name == "SECTION"
 
     # Test question structure.
     qlist = []
     for question in section.children:
         assert isinstance(question, Node)
-        assert question.name in ('NEW_QUESTION', 'CONSECUTIVE_QUESTION')
+        assert question.name in ("NEW_QUESTION", "CONSECUTIVE_QUESTION")
         assert len(question.children) == 1
         version = question.children[0]
         assert isinstance(version, Node)
-        assert version.name == 'VERSION'
+        assert version.name == "VERSION"
         question_text = version.children[1].strip()
         qlist.append(question_text.split()[0])
-        if 'must follow' in question_text:
-            assert question.name == 'CONSECUTIVE_QUESTION'
+        if "must follow" in question_text:
+            assert question.name == "CONSECUTIVE_QUESTION"
         else:
-            assert question.name == 'NEW_QUESTION'
-    assert qlist == [f'question{c}' for c in 'ABCDEFGHIJ'], qlist
+            assert question.name == "NEW_QUESTION"
+    assert qlist == [f"question{c}" for c in "ABCDEFGHIJ"], qlist
 
     # Test questions order
     latex = c.get_latex()
     questions = []
     i = 0
     while i != -1:
-        i = latex.find('question', i)
-        if i!= -1:
-            i += len('question')
-            questions.append(latex[i:i+1])
-    ordering = ''.join(questions)
-    assert 'DEFGHI' in ordering
-    assert ordering != ''.join(sorted(questions))
-    e0 = latex.find('ansE0')
-    e1 = latex.find('ansE1')
-    e2 = latex.find('ansE2')
-    e3 = latex.find('ansE3')
-    e4 = latex.find('ansE4')
-    f1 = latex.find('ansF1')
-    f2 = latex.find('ansF2')
-    f3 = latex.find('ansF3')
+        i = latex.find("question", i)
+        if i != -1:
+            i += len("question")
+            questions.append(latex[i : i + 1])
+    ordering = "".join(questions)
+    assert "DEFGHI" in ordering
+    assert ordering != "".join(sorted(questions))
+    e0 = latex.find("ansE0")
+    e1 = latex.find("ansE1")
+    e2 = latex.find("ansE2")
+    e3 = latex.find("ansE3")
+    e4 = latex.find("ansE4")
+    f1 = latex.find("ansF1")
+    f2 = latex.find("ansF2")
+    f3 = latex.find("ansF3")
     assert all(i != -1 for i in (e0, e1, e2, e3, e4, f1, f2, f3))
     assert max(f1, f2) < f3
     assert f1 < f3
     assert e0 < min(e1, e2, e3) < max(e1, e2, e3) < e4
-    assert '\n\n' not in latex[f1:f2]
-    assert '\n\n' in latex[f2:f3]
-    assert '\n\n' not in latex[e0:e1]
-    assert '\n\n' not in latex[e1:e2]
-    assert '\n\n' not in latex[e2:e3]
-    assert '\n\n' not in latex[e3:e4]
+    assert "\n\n" not in latex[f1:f2]
+    assert "\n\n" in latex[f2:f3]
+    assert "\n\n" not in latex[e0:e1]
+    assert "\n\n" not in latex[e1:e2]
+    assert "\n\n" not in latex[e2:e3]
+    assert "\n\n" not in latex[e3:e4]
     return latex
+
 
 def test_include():
     os.chdir(TEST_DIR)
-    c = load_ptyx_file('test_include.ptyx')
+    c = load_ptyx_file("test_include.ptyx")
     # Test for support of:
     # - no star at all at the beginning of the question (must be automatically added)
-    with open('exercises/ex1.txt') as f:
-        assert not f.read().startswith('*')
+    with open("exercises/ex1.txt") as f:
+        assert not f.read().startswith("*")
     # - a line break after the star. This should be Ok too.
-    with open('exercises/ex2.txt') as f:
-        assert f.read().startswith('*\n')
+    with open("exercises/ex2.txt") as f:
+        assert f.read().startswith("*\n")
     c.generate_syntax_tree()
     latex = c.get_latex()
     assert r"$2\times(-1)^2$" in latex
     assert "an other answer" in latex
 
+
 def test_include_glob():
     os.chdir(TEST_DIR)
-    c1 = load_ptyx_file('test_include.ptyx')
+    c1 = load_ptyx_file("test_include.ptyx")
     c1.generate_syntax_tree()
     latex1 = c1.get_latex()
-    c2 = load_ptyx_file('test_include_glob.ptyx')
+    c2 = load_ptyx_file("test_include_glob.ptyx")
     c2.generate_syntax_tree()
     latex2 = c2.get_latex()
     assert r"$2\times(-1)^2$" in latex2
@@ -152,16 +154,16 @@ def test_include_glob():
 
 
 def test_question_context():
-    c = load_ptyx_file('test_questions_context.ptyx')
+    c = load_ptyx_file("test_questions_context.ptyx")
     c.generate_syntax_tree()
     latex = c.get_latex()
 
-    for match in re.finditer(r'TEST\((\w+)=(\w+)\)', latex):
+    for match in re.finditer(r"TEST\((\w+)=(\w+)\)", latex):
         assert match.group(1) == match.group(2)
 
 
 def test_unicity_of_answers():
-    c = load_ptyx_file('test_unicity_of_answers.ptyx')
+    c = load_ptyx_file("test_unicity_of_answers.ptyx")
     c.generate_syntax_tree()
     try:
         c.get_latex()
@@ -184,6 +186,6 @@ def cleanup():
         (TEST_DIR / Path(tmp_filename)).unlink()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     test_MCQ_shuffling()
-    print('OK')
+    print("OK")
