d86855358292de98b340c140830b99c4e7442879
Nicolas Pourcelot <nicolas.pourcelot@gmail.com>
2023-12-04 13:59:10 +0100

    feat!: Change #* tag behaviour, taking into account previous evaluation.

--- tests/test_tags.py
+++ tests/test_tags.py
@@ -2,13 +2,24 @@ import os
 import re
 from os.path import dirname
 
+import pytest
+
 from ptyx.latex_generator import Compiler
 
 TEST_DIR = dirname(__file__)
 
 
+@pytest.fixture
+def compiler():
+    return Compiler()
+
+
+def parse(code: str, **kw) -> str:
+    return Compiler().parse(code=code, **kw)
+
+
 def test_SEED_SHUFFLE():
-    test = '''#SEED{16}Who said "Having nothing, nothing can he lose" ?
+    code = '''#SEED{16}Who said "Having nothing, nothing can he lose" ?
 
 \\begin{enumerate}
 #SHUFFLE
@@ -26,12 +37,10 @@ def test_SEED_SHUFFLE():
 \\end{enumerate}
 
 "The game is up."'''
-    c = Compiler()
-    latex = c.parse(code=test)
-    assert latex == result
+    assert parse(code) == result
 
 
-def test_SEED_SHUFFLE_2():
+def test_SEED_SHUFFLE_2(compiler):
     tests = [
         """#SEED{153}%
 #SHUFFLE
@@ -62,16 +71,15 @@ b
 c
 #END""",
     ]
-    c = Compiler()
     results = []
     for test in tests:
-        results.append(c.parse(code=test))
+        results.append(compiler.parse(code=test))
     assert results[0] == "%\nc\na\nb"
     assert results[1] == "%\n\nc\na\nb"
     assert results[2] == "%\nc\na\nb"
 
 
-def test_PICK():
+def test_PICK(compiler):
     test = """
     And the winner is:
     #PICK
@@ -84,145 +92,121 @@ def test_PICK():
     #ITEM
     4
     #END_PICK"""
-    c = Compiler()
-    c.read_code(test)
-    c.preparse()
+    compiler.read_code(test)
+    compiler.preparse()
     # Tweak seed.
-    c._state["seed"] = 1
-    c.generate_syntax_tree()
-    g = c.latex_generator
+    compiler._state["seed"] = 1
+    compiler.generate_syntax_tree()
+    g = compiler.latex_generator
     assert g.NUM == 0
 
     # Tweak seed again.
-    c._state["seed"] = 5
+    compiler._state["seed"] = 5
     assert g.NUM == 0
-    latex = c.get_latex()
+    latex = compiler.get_latex()
     latex = re.sub(r"\s+", " ", latex).strip()
     assert latex == "And the winner is: 3"
 
 
-def test_CASE():
-    test = (
-        "#CASE{0}first case#CASE{1}second case#CASE{2}third one#END#CASE{1} bonus#END this is something else."
-    )
-    result = "second case bonus this is something else."
-    c = Compiler()
-    latex = c.parse(code=test, PTYX_NUM=1)
-    assert latex == result
+def test_CASE(compiler):
+    code = "#CASE{0}1st case#CASE{1}2nd case#CASE{2}3rd one#END#CASE{1} bonus#END this is something else."
+    assert parse(code, PTYX_NUM=1) == "2nd case bonus this is something else."
 
 
 def test_IF_ELIF_ELSE():
-    test = (
+    code = (
         "#{a=1;}#IF{a==0}0#ELIF{a==1}1#ELSE{}2#END"
         "#{a=0;}#IF{a==0}0#ELIF{a==1}1#ELSE{}2#END#{a=2;}#IF{a==0}0#ELIF{a==1}1#ELSE{}2#END."
     )
-    result = r"10{}2."
-    c = Compiler()
-    latex = c.parse(code=test)
-    assert latex == result
+    assert parse(code) == r"10{}2."
 
 
-def test_MACRO():
-    test = r"#MACRO{a0}#IF{a==0}$a=0$#ELSE$a\neq 0$#END#END_MACRO#{a=0;}Initially #CALL{a0}#{a=2;}, but now #CALL{a0}."
-    result = r"Initially $a=0$, but now $a\neq 0$."
-    c = Compiler()
-    latex = c.parse(code=test)
-    assert latex == result
+def test_MACRO(compiler):
+    code = r"#MACRO{a0}#IF{a==0}$a=0$#ELSE$a\neq 0$#END#END_MACRO#{a=0;}Initially #CALL{a0}#{a=2;}, but now #CALL{a0}."
+    assert parse(code) == r"Initially $a=0$, but now $a\neq 0$."
 
 
 def test_TEST():
-    test = r"""#{hxA=2;}#{yA=3;}\fbox{#TEST{hxA==yA}{is in}{isn't in}}"""
-    result = r"""\fbox{isn't in}"""
-    c = Compiler()
-    latex = c.parse(code=test)
-    assert latex == result
+    code = r"""#{hxA=2;}#{yA=3;}\fbox{#TEST{hxA==yA}{is in}{isn't in}}"""
+    assert parse(code) == r"\fbox{isn't in}"
 
 
 def test_MUL():
-    # Test 1
-    test = r"""
+    code = r"""
 #PYTHON
-a=-8
-b=4
-c=5
-d=8
+a, b, c, d = -8, 4, 5, 8
 #END
 $\dfrac{#a#*(#{c*x+d})#-#{a*x+b}#*#c}{(#{c*x+d})^2}$"""
     result = r"""
 $\dfrac{-8\times (5 x + 8)-\left(- 8 x + 4\right)\times 5}{(5 x + 8)^2}$"""
     alternative_result = r"""
 $\dfrac{-8\times (5 x + 8)-\left(4 - 8 x\right)\times 5}{(5 x + 8)^2}$"""
-    c = Compiler()
-    latex = c.parse(code=test)
-    assert latex == result or latex == alternative_result
+    assert (latex := parse(code)) == result or latex == alternative_result
 
-    # Test 2
-    test = "2#*3"
-    result = r"2\times 3"
-    c = Compiler()
-    latex = c.parse(code=test)
-    assert latex == result
+    assert parse("2#*3") == r"2\times 3"
 
 
-def test_MUL_SUB_ADD_expr():
-    test = r"""
+def test_SUB_ADD_expr():
+    code = r"""
+#PYTHON
+a, b, c, d = 6, 6, 5, -4
+#END
+$#a#-#{b*x+c}$
+$#a#+#d$"""
+    result = r"""
+$6-\left(6 x + 5\right)$
+$6-4$"""
+    assert parse(code) == result
+
+
+def test_MUL_expr():
+    code = r"""
 #PYTHON
 a = 6
 b = 6
 c = 5
 d = -4
+e = 0
+f = -1
+g = 1
 #END
 $#a#*#{b*x+c}$
 $#a#*#d$
 $#d#*#a$
 $#d#*#x$
-$#a#-#{b*x+c}$
-$#a#+#d$"""
+#e#*#x#+#c
+#g #* #{x + a}
+#d #+ #g #* #x
+#d #+ #f #* #x
+"""
     result = r"""
 $6\left(6 x + 5\right)$
 $6\times \left(-4\right)$
 $-4\times 6$
 $-4x$
-$6-\left(6 x + 5\right)$
-$6-4$"""
-    c = Compiler()
-    latex = c.parse(code=test)
-    assert latex == result
+0+5
+  \left(x + 6\right)
+-4  +  x
+-4  -  x
+"""
+    assert parse(code) == result
 
 
 def test_EVAL_abs():
-    test = r"$#{abs(-5)}$"
-    result = r"$5$"
-    c = Compiler()
-    latex = c.parse(code=test)
-    assert latex == result
+    assert parse(r"$#{abs(-5)}$") == r"$5$"
 
 
 def test_EVAL_rounding():
-    test = r"#[2]{2/3}"
-    result = r"0,67"
-    c = Compiler()
-    latex = c.parse(code=test)
-    assert latex == result
-    test = r"#[2,.]{2/3}"
-    result = r"0.67"
-    c = Compiler()
-    latex = c.parse(code=test)
-    assert latex == result
+    assert parse(r"#[2]{2/3}") == r"0,67"
+    assert parse(r"#[2,.]{2/3}") == r"0.67"
 
 
 def test_ADD():
-    test = r"""
-#PYTHON
-a = 2
-b = 3
+    code = r"""#PYTHON
+a, b = 2, 3
 #END
 $#a#+\dfrac{#b}{x}$"""
-    result = r"""
-$2+\dfrac{3}{x}$"""
-    c = Compiler()
-    latex = c.parse(code=test)
-    assert latex == result
+    assert parse(code) == "\n" r"$2+\dfrac{3}{x}$"
 
 
 def test_EVAL_mul_flag():
@@ -252,37 +236,32 @@ def test_EVAL_mul_flag():
     assert latex.replace(" ", "").strip() == target
 
 
-def test_rand_pick():
-    test = r"""
+def test_rand_select_pick():
+    code = r"""
     #SEED{187}
     #PYTHON
     values = [1,2,3,4]
     #END
-    #[rand]values is #[select]values ?
+    #[rand]values is #[select]values?
     """
-    target = "3is1?"
-    c = Compiler()
-    latex = c.parse(code=test)
-    assert latex.replace(" ", "").strip() == target
+    assert parse(code).strip() == "3 is 1?"
 
 
 def test_INCLUDE():
     os.chdir(TEST_DIR)
-    test = "#SEED{99}First, $a=#{a=7}$#INCLUDE{include_example.txt}Last, $a=#a$ still."
+    code = "#SEED{99}First, $a=#{a=7}$#INCLUDE{include_example.txt}Last, $a=#a$ still."
     result = """First, $a=7$
 This is an example of a file
 that can be included using INCLUDE tag.
 It may contain some pTyX code...
 Note that if $a=1$, then $a+1=2$.
 Last, $a=7$ still."""
-    c = Compiler()
-    latex = c.parse(code=test)
-    assert latex == result
+    assert parse(code) == result
 
 
 def test_INCLUDE_newline():
     os.chdir(TEST_DIR)
-    test = """#SEED{99}
+    code = """#SEED{99}
 First, $a=#{a=7}$
 #INCLUDE{include_example.txt}
 Last, $a=#a$ still."""
@@ -295,89 +274,79 @@ It may contain some pTyX code...
 Note that if $a=1$, then $a+1=2$.
 
 Last, $a=7$ still."""
-    c = Compiler()
-    latex = c.parse(code=test)
-    assert latex == result
+    assert parse(code) == result
 
 
 def test_VERBATIM_tag():
-    c = Compiler()
-    test = r"""
+    code = r"""
 #VERBATIM
 def pi2():
     return "$\pi^2$"
 #END
 """
-    latex = c.parse(code=test)
-    assert latex == (
+    assert parse(code) == (
         r'\texttt{def~pi2():\linebreak\phantom{}~~~~return~"\$\textbackslash{}pi\textasciicircum{}2\$"}' "\n"
     )
 
 
 def test_PYTHON_tag():
-    c = Compiler()
-    test = r"""
+    code = r"""
 #PYTHON
 a = 2 # test for comment support
 #END
 #a
 """
-    latex = c.parse(code=test)
-    assert latex == "\n2\n"
+    assert parse(code) == "\n2\n"
 
 
 def test_PRINT_tag(capfd):
-    c = Compiler()
-    test = r"""
+    code = r"""
 #SEED{0}
 #PRINT{Hello}
 """
-    latex = c.parse(code=test)
+    latex = parse(code)
     out, err = capfd.readouterr()
     assert out == "Hello\n"
+    assert latex.strip() == ""
 
 
 def test_PRINT_hash_symbol(capfd):
-    c = Compiler()
-    test = r"""
+    code = r"""
 #SEED{0}
 #PRINT{Hello ## ##}
 """
-    latex = c.parse(code=test)
+    latex = parse(code)
     out, err = capfd.readouterr()
     assert out == "Hello # #\n"
     assert latex == "\n\n\n"
 
 
 def test_PRINT_hash_symbol2(capfd):
-    c = Compiler()
-    test = r"""
+    code = r"""
 #SEED{0}
 #{a=7;}
 #PRINT{Hello ##a ##}
 """
-    latex = c.parse(code=test)
+    latex = parse(code)
     out, err = capfd.readouterr()
     assert out == "Hello #a #\n"
     assert latex == "\n\n\n\n"
 
 
 def test_PRINT_EVAL_tag(capfd):
-    c = Compiler()
-    test = r"""
+    code = r"""
 #SEED{0}
 #{a=7;}Hi #a!
 #PRINT_EVAL{Hello #a ##}
 """
-    latex = c.parse(code=test)
+    latex = parse(code)
     out, err = capfd.readouterr()
     assert out == "Hello 7 #\n"
     assert latex == "\n\nHi 7!\n\n"
 
 
-def test_ASK_ASK_ONLY_ANS_ANSWER_tag():
-    c = Compiler()
-    test = r"""
+def test_ASK_ASK_ONLY_ANS_ANSWER_tag(compiler):
+    code = r"""
 #SEED{5}Hello!
 
 \dotfill
@@ -396,10 +365,7 @@ Question:
 
 Bye!
 """
-    tex = c.parse(code=test)
-    assert (
-        tex
-        == r"""
+    result = r"""
 Hello!
 
 \dotfill
@@ -411,12 +377,8 @@ Question:
 
 Bye!
 """
-    )
-    c.reset()
-    tex = c.parse(code=test, PTYX_WITH_ANSWERS=True)
-    assert (
-        tex
-        == r"""
+    assert compiler.parse(code=code) == result
+    result = r"""
 Hello!
 
 \dotfill
@@ -428,4 +390,5 @@ That was easy!%
 
 Bye!
 """
-    )
+    compiler.reset()
+    assert compiler.parse(code=code, PTYX_WITH_ANSWERS=True) == result
